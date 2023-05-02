580167e2916b51f84dbd709d2d4a5648c879d756
madhavi <madhavilatha.008@gmail.com>
2023-05-02 11:10:07 -0500

    terraform

---
+++ loops/count/main.tf
@@ -0,0 +1,11 @@
+resorce "null_resource" "fruits" {
+  provisioner "local-exec" {
+    command = " echo ${ leanth(var.fruits)}"
+  }}
+variable "fruits"{
+  default={
+    apple = 10
+    orange = 200
+    banana = 100
+  }
+}
